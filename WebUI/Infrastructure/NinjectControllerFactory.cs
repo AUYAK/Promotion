779cf85485129893bdf82a78667ab7bdbfef569f
BMK\asu <asu@bmk.by>
2018-02-27 09:44:42 +0300

    initial commit

---
+++ WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        private IKernel _ninjectKernel;
+        public NinjectControllerFactory() {
+            _ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+        }
+    }
+}
