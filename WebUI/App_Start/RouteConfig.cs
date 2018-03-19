9e95e021d31701b2c26f60dfd5d5d860d23946ec
BMK\asu <asu@bmk.by>
2018-03-19 14:52:30 +0300

    Edits in RouteConfig

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -12,6 +12,11 @@ namespace WebUI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(name: null, url: "", defaults: new { controller = "Blurbs", action = "List",category=(string)null,page=1});
+            routes.MapRoute(name: null, url: "Page{page}", defaults: new { controller = "Blurbs", action = "List", category = (string)null},constraints:new{page=@"\d+"});
+            routes.MapRoute(name: null, url: "{category}", defaults: new { controller = "Blurbs", action = "List", page = 1 });
+            routes.MapRoute(name: null, url: "{category}/Page{page}", defaults: new { controller = "Blurbs", action = "List" }, constraints: new { page = @"\d+" });
+
             routes.MapRoute(
                 name: null,
                 url: "Page{page}",
