405ac2ed7ff303a172bf15217d7a5d7dfa0c3646
BMK\asu <asu@bmk.by>
2018-03-19 16:31:32 +0300

    Added NavController. Edits in UI.

---
+++ WebUI/Controllers/NavController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IBlurbRepository repository;
+        // GET: Nav
+        public NavController(IBlurbRepository repo)
+        {
+            repository = repo;
+        }
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = (IEnumerable<string>)repository.Blurbs.Select(x => x.Category.Name).Distinct();
+            return PartialView(categories);
+        }
+    }
+}
