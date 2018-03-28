649d634808589fae2f9c0a74e0490ae416005d88
BMK\asu <asu@bmk.by>
2018-03-28 09:31:08 +0300

    Edits in RouteConfig.cs

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -11,22 +11,22 @@ namespace WebUI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            //routes.MapRoute(name: null, url: "", defaults: new { controller = "Blurbs", action = "List", category = (string)null, page = 1 });
-            //routes.MapRoute(name: null, url: "Page{page}", defaults: new { controller = "Blurbs", action = "List", category = (string)null }, constraints: new { page = @"\d+" });
-            //routes.MapRoute(name: null, url: "{category}", defaults: new { controller = "Blurbs", action = "List", page = 1 });
-            //routes.MapRoute(name: null, url: "{category}/Page{page}", defaults: new { controller = "Blurbs", action = "List" }, constraints: new { page = @"\d+" });
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(name: null, url: "", defaults: new { controller = "Blurbs", action = "List", category = (string)null, page = 1 });
+            routes.MapRoute(name: null, url: "Page{page}", defaults: new { controller = "Blurbs", action = "List", category = (string)null }, constraints: new { page = @"\d+" });
+            routes.MapRoute(name: null, url: "{category}", defaults: new { controller = "Blurbs", action = "List", page = 1 });
+            routes.MapRoute(name: null, url: "{category}/Page{page}", defaults: new { controller = "Blurbs", action = "List" }, constraints: new { page = @"\d+" });
 
-            //routes.MapRoute(
-            //    name: null,
-            //    url: "Page{page}",
-            //    defaults: new { Controller = "Blurbs", action = "List" }
-            //    );
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Blurbs", action = "List" }
+                );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Blurbs", action = "List", id = UrlParameter.Optional }
-            );
+                defaults:new {id=UrlParameter.Optional }
+                );
         }
     }
 }
