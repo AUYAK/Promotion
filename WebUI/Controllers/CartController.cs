4f533ed105c90fc1878edd202b16c43cf4e1ac39
BMK\asu <asu@bmk.by>
2018-03-29 12:09:08 +0300

    Added Cart  use-case

---
+++ WebUI/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl });
+        }
+        public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index","Cart",new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(int productID, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (product != null)
+            {
+                GetCart().RemoveAll(product);
+            }
+            return RedirectToAction("Index","Cart", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session.Add("Cart", cart);
+            }
+            return cart;
+        }
+    }
+}
---
+++ WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using System;
+
+namespace WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public String ReturnUrl { get; set; }
+     }
+}
