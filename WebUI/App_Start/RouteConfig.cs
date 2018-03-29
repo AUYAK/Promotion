54fcae42a8b407468188faa1fbeb38c824b7351a
BMK\asu <asu@bmk.by>
2018-03-29 12:09:32 +0300

    Routeconfig edits

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -16,10 +16,15 @@ namespace WebUI
             routes.MapRoute(name: null, url: "Page{page}", defaults: new { controller = "Blurbs", action = "List", category = (string)null }, constraints: new { page = @"\d+" });
             routes.MapRoute(name: null, url: "{controller}", defaults: new { action = "List"});
             routes.MapRoute(name: null, url: "{controller}/Page{page}", defaults: new { action = "List"},constraints: new {page=@"\d+" });
-            routes.MapRoute(name: null, url: "{controller}/{category}", defaults: new { action = "List" });
+            routes.MapRoute(name: null, url: "{controller}/{action}", defaults: new { action = "List", category = (string)null,page=1 }, constraints: new { controller = "Product",action="List" });
+            routes.MapRoute(name: null, url: "{controller}/{action}", defaults: new { action = "List", category = (string)null,page=1 }, constraints: new { controller = "Blurb",action="List" });
+            routes.MapRoute(name: null, url: "{controller}/{category}", defaults: new { action = "List" },constraints: new {controller="Product"});
+            routes.MapRoute(name: null, url: "{controller}/{category}", defaults: new { action = "List" }, constraints: new { controller = "Blurb" });
+            routes.MapRoute(name: null, url: "{controller}/{action}");
             routes.MapRoute(name: null, url: "{controller}/{category}/Page{page}", defaults: new { action = "List"},constraints: new {page=@"\d+" });
             routes.MapRoute(name: null, url: "{category}", defaults: new { controller = "Blurbs", action = "List", page = 1 });
             routes.MapRoute(name: null, url: "{category}/Page{page}", defaults: new { controller = "Blurbs", action = "List" }, constraints: new { page = @"\d+" });
+          
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
