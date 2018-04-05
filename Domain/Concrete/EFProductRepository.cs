cf4d79038f8918fed76fbb3a084a6461c864583d
BMK\asu <asu@bmk.by>
2018-04-05 14:48:56 +0300

    Admin Usecase created

--- Domain/Concrete/EFProductRepository.cs
+++ Domain/Concrete/EFProductRepository.cs
@@ -11,6 +11,26 @@ namespace Domain.Concrete
     public class EFProductRepository:IProductRepository
     {
         private EFDbContext context = new EFDbContext();
-        public IQueryable<Product> Products { get { return context.Products; }} 
+        public IQueryable<Product> Products { get { return context.Products; }}
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Price = product.Price;
+                    dbEntry.CategoryID = product.CategoryID;
+                    dbEntry.Description = product.Description;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
---
+++ UnitTests/AdminControllerUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebUI.Controllers;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AdminControllerUnitTests
+    {
+        [TestMethod]
+        public void Index_Returns_All_Products()
+        {
+            //Arrange - create mock
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new Product[] { new Product { ProductID = 1, Name = "P1"},new Product {ProductID=2,Name="P2"}, new Product {ProductID=3,Name="P3"}}.AsQueryable());
+            //Act
+            AdminController target = new AdminController(mock.Object);
+            Product[] result = ((IEnumerable<Product>)target.Index().Model).ToArray();
+            //Assert block
+            Assert.AreEqual(result.Length,3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+        }
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+            //Arrange - create and setup mock
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] { new Product { ProductID = 1, Name = "P1" }, new Product { ProductID = 2, Name = "P2" }, new Product { ProductID = 3, Name = "P3" } }.AsQueryable());
+            //Arrange - create AdminController
+            AdminController controller = new AdminController(mock.Object);
+            //Act - get product
+            Product product = (Product)controller.Edit(2).Model;
+            //Assert
+            Assert.AreEqual(2, product.ProductID);
+            Assert.AreEqual("P2", product.Name);
+
+        }
+        [TestMethod]
+        public void Cannot_Edit_NonExistent_Product()
+        {
+            //Arrange - create and setup mock
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] { new Product { ProductID = 1, Name = "P1" }, new Product { ProductID = 2, Name = "P2" }, new Product { ProductID = 3, Name = "P3" } }.AsQueryable());
+            //Arrange - create AdminController
+            AdminController controller = new AdminController(mock.Object);
+            //Act - get product
+            Product product = (Product)controller.Edit(5).Model;
+            //Assert
+            Assert.IsNull(product);
+        }
+    }
+}
